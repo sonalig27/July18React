51d2588ddc4de3e528c67e0e4b904a4cb78bfcfe
sonalig27 <ganatra.sonali@gmail.com>
2021-07-18 23:21:28 -0400

    changed user information

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,22 +1,25 @@
-import React from 'react';
-import { Jumbotron, Container} from 'reactstrap';
-import NavBar from '../Navbar/Navbar';
+import React from "react";
+import { Jumbotron, Container } from "reactstrap";
+import NavBar from "../Navbar/Navbar";
 
 function Contact() {
   return (
     <div className="App">
-     <div>
-      <Jumbotron fluid>
-         <NavBar />
-        <Container fluid>
-          <h1 className="display-3">My Contact Details</h1>
-          <p className="lead">Here are my contact details </p>
-          <p className="lead">Email : sachin@wynisco.com my email </p>
-          <p className="lead">Phone : 732 874 0188 </p>
-          <p className="lead">Typical Wake up / Sleep Hours : Other than bootcamp. I will available morning 7AM to 11AM EST via slack or whatsapp Changed  </p>
-        </Container>
-      </Jumbotron>
-    </div>
+      <div>
+        <Jumbotron fluid>
+          <NavBar />
+          <Container fluid>
+            <h1 className="display-3">My Contact Details</h1>
+            <p className="lead">Here are my contact details </p>
+            <p className="lead">Email : ganatra.sonali@gmail.com my email </p>
+            <p className="lead">Phone : 732 874 0188 </p>
+            <p className="lead">
+              Typical Wake up / Sleep Hours : Other than bootcamp. I will
+              available morning 7AM to 11AM EST via slack or whatsapp Changed{" "}
+            </p>
+          </Container>
+        </Jumbotron>
+      </div>
     </div>
   );
 }
--- src/Components/Homeworks/index.js
+++ src/Components/Homeworks/index.js
@@ -1,49 +1,52 @@
-import React from 'react';
-import { Jumbotron, Container} from 'reactstrap';
-import NavBar from '../Navbar/Navbar'
-import { Table } from 'reactstrap';
+import React from "react";
+import { Jumbotron, Container } from "reactstrap";
+import NavBar from "../Navbar/Navbar";
+import { Table } from "reactstrap";
 // import { Link } from 'react-router-dom' ;
 
 function Homeworks() {
   return (
     <div className="App">
-     <div>
-      <Jumbotron fluid>
-        <Container fluid>
-          <h1 className="display-3">WELCOME TO BOOTCAMP - MAY 24 EDITION</h1>
-          <p className="lead">My Name is Sachin Rajgire and I am student here </p>
-          <NavBar />
+      <div>
+        <Jumbotron fluid>
+          <Container fluid>
+            <h1 className="display-3">WELCOME TO BOOTCAMP - MAY 24 EDITION</h1>
+            <p className="lead">
+              My Name is Sonali Ganatra and I am a student here in EST
+            </p>
+            <NavBar />
 
-          <Table dark>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Type</th>
-          <th>Description</th>
-          <th>Link</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Homework1</td>
-          <td>Git Practice. Provide link of your repo here </td>
-          <td><a href="https://github.com"> Link</a> </td>
-       
-      </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Homework 2</td>
-          <td>Javascript - Here is my codepen link with solutions</td>
-          <td><a href="https://codepen.io/wynisco/pen/ExWmXzd"> Link</a> </td>
-       
-      </tr>
-        
-      </tbody>
-    </Table>
-        </Container>
-      </Jumbotron>
-    </div>
+            <Table dark>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Type</th>
+                  <th>Description</th>
+                  <th>Link</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>Homework1</td>
+                  <td>Git Practice. Provide link of your repo here </td>
+                  <td>
+                    <a href="https://github.com"> Link</a>{" "}
+                  </td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>Homework 2</td>
+                  <td>Javascript - Here is my codepen link with solutions</td>
+                  <td>
+                    <a href="https://codepen.io/wynisco/pen/ExWmXzd"> Link</a>{" "}
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+          </Container>
+        </Jumbotron>
+      </div>
     </div>
   );
 }
